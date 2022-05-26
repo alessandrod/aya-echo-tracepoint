33a1aee2eaa7503615a444ffa574dfba2be943f9
Alessandro Decina <alessandro.d@gmail.com>
2022-05-26 08:43:22 +0000

    Fix artifact name + verifier error

--- echo-ebpf/src/main.rs
+++ echo-ebpf/src/main.rs
@@ -21,7 +21,7 @@ pub struct Buf {
 #[map]
 pub static mut BUF: PerCpuArray<Buf> = PerCpuArray::with_max_entries(1, 0);
 
-#[tracepoint(name="echo")]
+#[tracepoint(name = "echo")]
 pub fn echo_trace_open(ctx: TracePointContext) -> c_long {
     match try_echo_trace_open(ctx) {
         Ok(ret) => ret,
@@ -34,7 +34,7 @@ fn try_echo_trace_open(ctx: TracePointContext) -> Result<c_long, c_long> {
     // sudo cat /sys/kernel/debug/tracing/events/syscalls/sys_enter_open/format
     const FILENAME_OFFSET: usize = 24;
     let filename_addr: u64 = unsafe { ctx.read_at(FILENAME_OFFSET)? };
-    
+
     // get the map-backed buffer that we're going to use as storage for the filename
     let buf = unsafe { BUF.get_mut(0) }.ok_or(0)?;
 
@@ -44,8 +44,10 @@ fn try_echo_trace_open(ctx: TracePointContext) -> Result<c_long, c_long> {
         core::str::from_utf8_unchecked(&buf.buf[..len])
     };
 
-    // log the filename
-    info!(&ctx, "open {}", filename);
+    if filename.len() < 512 {
+        // log the filename
+        info!(&ctx, "open {}", filename);
+    }
 
     Ok(0)
 }
