5e459b1a90226eb16d53b12ffd1184a2e688e5a7
Davide Bertola <dade@dadeb.it>
2022-07-30 16:15:14 +0200

    update to per-cpu-array api change

--- echo-ebpf/src/main.rs
+++ echo-ebpf/src/main.rs
@@ -3,7 +3,7 @@
 
 use aya_bpf::{
     cty::c_long,
-    helpers::bpf_probe_read_user_str,
+    helpers::bpf_probe_read_user_str_bytes,
     macros::{map, tracepoint},
     maps::PerCpuArray,
     programs::TracePointContext,
@@ -21,7 +21,7 @@ pub struct Buf {
 #[map]
 pub static mut BUF: PerCpuArray<Buf> = PerCpuArray::with_max_entries(1, 0);
 
-#[tracepoint(name="echo")]
+#[tracepoint(name = "echo")]
 pub fn echo_trace_open(ctx: TracePointContext) -> c_long {
     match try_echo_trace_open(ctx) {
         Ok(ret) => ret,
@@ -34,14 +34,19 @@ fn try_echo_trace_open(ctx: TracePointContext) -> Result<c_long, c_long> {
     // sudo cat /sys/kernel/debug/tracing/events/syscalls/sys_enter_open/format
     const FILENAME_OFFSET: usize = 24;
     let filename_addr: u64 = unsafe { ctx.read_at(FILENAME_OFFSET)? };
-    
+
     // get the map-backed buffer that we're going to use as storage for the filename
-    let buf = unsafe { BUF.get_mut(0) }.ok_or(0)?;
+    let buf = unsafe {
+        let ptr = BUF.get_ptr_mut(0).ok_or(0)?;
+        &mut *ptr
+    };
 
     // read the filename
     let filename = unsafe {
-        let len = bpf_probe_read_user_str(filename_addr as *const u8, &mut buf.buf)?;
-        core::str::from_utf8_unchecked(&buf.buf[..len])
+        core::str::from_utf8_unchecked(bpf_probe_read_user_str_bytes(
+            filename_addr as *const u8,
+            &mut buf.buf,
+        )?)
     };
 
     // log the filename
